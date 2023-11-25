c5385d489588a3d62a7395139e26f82bb8d85243
Datta-070 <chinmaydatt003@gmail.com>
2023-11-25 21:22:06 +0530

    Pushed

---
+++ script.js
@@ -0,0 +1,90 @@
+const form= document.getElementById("form");
+const recordscontainer= document.getElementById("records");
+const createButton= document.querySelector("#form button");
+
+const employeesList=[];
+
+let empid=1000;
+
+const onSubmitForm = (event) =>{
+    event.preventDefault();
+     
+    const employee = {
+        name:form.name.value,
+        empid:++empid,
+        salary:form.salary.value,
+        team:form.team.value,
+        role:form.role.value,
+        companyname:form.companyname.value
+    }
+    form.reset();
+    addNewEmployeeRecord(employee);
+}
+
+function deleteRecord(event){
+    const deleteButton = event.target;
+    const record = deleteButton.parentNode.parentNode;
+    record.remove();
+
+    const currentEmployeeId = parseInt(deleteButton.getAttribute("data-empid"));
+
+    for(let i=0;i<employeesList.length;i++){
+        if(employeesList[i].empid===currentEmployeeId){
+            employeesList.splice(i,1);
+            break;
+        }
+    }
+}
+
+function editRecord(event){
+    const editButton = event.target;
+    const currentEmployeeId = parseInt(editButton.getAttribute("data-empid"));
+
+    for(let i=0;i<employeesList.length;i++){
+        if(currentEmployeeId===employeesList[i].empid){
+            fillFormWithData(employeesList[i]);
+            break;
+        }
+    }
+}
+
+function fillFormWithData(employee){
+    for(let key in employee){
+        if(key !== "empId"){
+        form[key].value=employee[key];
+        }
+    }
+    createButton.innerText="Update Employee";
+}
+
+function addNewEmployeeRecord(employee){
+    const record=document.createElement("tr");
+
+    for(let key in employee){
+        const cell=document.createElement("td");
+        cell.innerText=employee[key];
+        record.appendChild(cell);
+    }
+    const optionsCell=document.createElement("td");
+    
+    const editIcon=document.createElement("span");
+    editIcon.className="material-icons icon";
+    editIcon.innerText="edit";
+    editIcon.setAttribute("data-empid", employee.empid);
+    editIcon.addEventListener("click", editRecord)
+
+    const deleteIcon=document.createElement("span");
+    deleteIcon.className="material-icons icon";
+    deleteIcon.innerText="delete";
+    deleteIcon.setAttribute("data-empid", employee.empid);
+    deleteIcon.addEventListener("click", deleteRecord);
+
+    optionsCell.append(editIcon,deleteIcon);
+    record.append(editIcon,deleteIcon);
+
+    recordscontainer.appendChild(record);
+    //add newly created employee to global employees list.
+    employeesList.push(employee);   
+}
+
+form.addEventListener("submit", onSubmitForm);
